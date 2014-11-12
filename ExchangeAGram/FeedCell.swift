ab527d35658896f1531b67b1ce0b0bba3613ad49
Arno Smit <a.smit@mac.com>
2014-11-12 10:10:47 +0100

    first commit

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by ApPeL on 2014/11/10.
+//  Copyright (c) 2014 Namib Lost in Sweden. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
---
+++ ExchangeAGram/FeedItem.swift
@@ -0,0 +1,18 @@
+//
+//  FeedItem.swift
+//  ExchangeAGram
+//
+//  Created by ApPeL on 2014/11/12.
+//  Copyright (c) 2014 Namib Lost in Sweden. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc (FeedItem)
+class FeedItem: NSManagedObject {
+
+    @NSManaged var caption: String
+    @NSManaged var image: NSData
+
+}
