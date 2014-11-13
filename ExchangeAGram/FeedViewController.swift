b61535bdc1a5f443c7549f81aec35a1f79fd2ecd
Arno Smit <a.smit@mac.com>
2014-11-13 08:18:12 +0100

    updated with working feeditem

--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -11,109 +11,110 @@ import MobileCoreServices
 import CoreData
 
 class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var feedArray: [AnyObject] = []
+    var feedArray:[AnyObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the 
+        
+        // Do any additional setup after loading the view.
+        
         let request = NSFetchRequest(entityName: "FeedItem")
         let appDelegate:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
         let context:NSManagedObjectContext = appDelegate.managedObjectContext!
         feedArray = context.executeFetchRequest(request, error: nil)!
+        
+        collectionView.reloadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    
     @IBAction func snapBarButtonItemTapped(sender: UIBarButtonItem) {
+        
+        let imagePickerContoller = UIImagePickerController()
+        var presentableController: UIViewController = imagePickerContoller
+        let mediaTypes:[AnyObject] = [kUTTypeImage]
+        imagePickerContoller.mediaTypes = mediaTypes
+        imagePickerContoller.allowsEditing = false
+        
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-            var cameraController = UIImagePickerController()
-            cameraController.delegate = self
-            cameraController.sourceType = UIImagePickerControllerSourceType.Camera
             
-            let mediaTypes:[AnyObject] = [kUTTypeImage]
-            cameraController.mediaTypes = mediaTypes
-            cameraController.allowsEditing = false
-            
-            self.presentViewController(cameraController, animated: true, completion: nil)
+            imagePickerContoller.sourceType = UIImagePickerControllerSourceType.Camera
         }
         else if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
-            var photoLibraryController = UIImagePickerController()
-            photoLibraryController.delegate = self
-            photoLibraryController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
             
-            let mediaTypes:[AnyObject] = [kUTTypeImage]
-            photoLibraryController.mediaTypes = mediaTypes
-            photoLibraryController.allowsEditing = false
-            
-            self.presentViewController(photoLibraryController, animated: true, completion: nil)
+            imagePickerContoller.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         }
         else {
-            var alertController = UIAlertController(title: "Error", message: "Your device does not support the camera or photo library", preferredStyle: UIAlertControllerStyle.Alert)
-            alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-            
-            self.presentViewController(alertController, animated: true, completion: nil)
+            var alertController  = UIAlertController(title: "Alert", message: "This device doesn't supoort Camera or Photo Library", preferredStyle: .Alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentableController = alertController
         }
+        
+        self.presentViewController(presentableController, animated: true, completion: nil)
     }
     
-    // UIImagePicker controller delegate
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        let image = info[UIImagePickerControllerOriginalImage] as UIImage
-        let imageData = UIImageJPEGRepresentation(image, 1.0)
-        
-        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
-        let managedObjectContext = appDelegate.managedObjectContext
-        let entityDescription = NSEntityDescription.entityForName("FeedItem", inManagedObjectContext: managedObjectContext!)
-        
-        var feedItem = FeedItem(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
-        
-        feedItem.image = imageData
-        feedItem.caption = "Feed item caption"
-        appDelegate.saveContext()
-        
-        feedArray.append(feedItem)
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
-        self.collectionView.reloadData()
-    }
+    //UICollectionViewDataSource
     
-    // UICollectionViewDataSource
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         return feedArray.count
     }
     
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        var cell:FeedCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as FeedCell
         
+        var cell:FeedCell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageCell", forIndexPath: indexPath) as FeedCell
+        
+        //Add data in feedcell
         let thisItem = feedArray[indexPath.row] as FeedItem
-        
         cell.imageView.image = UIImage(data: thisItem.image)
         cell.captionLabel.text = thisItem.caption
         
         return cell
     }
-
+    
+    
+    //UICollectionViewDelegate
+    
+    //UIImagePickerControllerDelegate
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as UIImage
+        
+        let imageData = UIImageJPEGRepresentation(image, 1.0)
+        
+        //Set up Core Date reference
+        let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
+        let entityDescription = NSEntityDescription.entityForName("FeedItem", inManagedObjectContext: managedObjectContext!)
+        let feedItem = FeedItem(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
+        
+        //Set properties
+        feedItem.image = imageData
+        feedItem.caption = "Arno"
+        
+        
+        //save to Core Data
+        (UIApplication.sharedApplication().delegate as AppDelegate).saveContext()
+        
+        feedArray.append(feedItem)
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        self.collectionView.reloadData()
+    }
+    
+    //UINavigationControllerDelegate
+    
 }
